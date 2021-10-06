df47c923cda1213cb74ad18c68eb201401acfb89
buihien224 <buihien224@gmail.com>
2021-10-06 10:46:31 +0700

    ..

--- gsi.sh
+++ gsi.sh
@@ -1,10 +1,18 @@
-codename="picasso"
-bhlnk="https://github.com/buihien224/host/raw/main/dynamic1.zip"
-scpt="tmp/META-INF/com/google/android/updater-script"
+#!/bin/sh
 
-#####
+codename="picasso"
+bhlnk="https://github.com/buihien224/host/raw/main/picasso.zip"
+scpt="tmp/META-INF/com/google/android/updater-script"
 user=$(whoami)
+mod=0
+#####
 cd ~/
+if [[ -d "output_picasso" ]]
+then cd output_picasso
+else
+mkdir output_picasso
+cd output_picasso
+fi
 pwd=$(pwd)
 echo "Working dir : "$pwd""
 echo ""
@@ -17,11 +25,8 @@ read gsi
 echo -e "\e[1;33mYour Device Is $codename and \e[0m"
 sleep 0.5
 ######
-if [[ -f "system.img" ]]
-then
-    echo -e "\e[1;33mGenerating..........\e[0m"
-    systemsize=$(find "system.img" -printf "%s")
-    echo "check if the template exists"
+check ()
+{
     if [[ -f "dynamic1.zip" ]]
     then
       echo "extracting template"
@@ -36,22 +41,86 @@ then
          exit 1
          fi
      fi
+}
+
+comp ()
+{
+   if [[ -f "system.img" ]]
+   then 
+      if [[ -f "vendor.img" ]]
+      then mod=2 
+      else mod=1 
+      fi
+    else 
+       echo " No image found" 
+       exit 1
+    fi
+
+}
+
+mksys()
+{
+    rm tmp/vendor_op_list
+    echo -e "\e[1;33mGenerating system flashable\e[0m"
+    systemsize=$(find "system.img" -printf "%s")
     echo -e "\e[1;33mCoping system.img...\e[0m"
     cp system.img tmp
     sed -i "s/test/$codename/g" $scpt 
     sed -i "s/160600/$systemsize/g" tmp/dynamic_partitions_op_list
-    echo -e "\e[1;33mDONE........\e[0m"
+    echo "set_progress(1.000000);" >> $scpt
+    echo -e "\e[1;33mZipping........\e[0m"
     for dir in tmp; do
     ( cd "$dir" && zip -r ../"$gsi"_"$codename".zip . )
     done
+    rm -r tmp
     echo -e "\e[1;32mDone Flash "$gsi"_"$codename".zip\e[0m"
     echo -e "\e[1;32mIn Custom Recovery\e[0m"
-    
+    sleep 5
+    exit 1
+}
+
+mkvendor()
+{
+    rm tmp/vendor_op_list
+    echo -e "\e[1;33mGenerating system and vendor flashable\e[0m"
+    systemsize=$(find "system.img" -printf "%s")
+    vendorsize=$(find "vendor.img" -printf "%s")
+    echo -e "\e[1;33mCoping system.img...\e[0m"
+    cp system.img tmp
+    echo -e "\e[1;33mCoping vendor.img...\e[0m"
+    cp vendor.img tmp
+    sed -i "s/test/$codename/g" $scpt 
+    sed -i "s/160600/$systemsize/g" tmp/dynamic_partitions_op_list
+    sed -i "s/010120/$vendorsize/g" tmp/vendor_op_list
+    echo "assert(update_dynamic_partitions(package_extract_file("vendor_op_list")));
+package_extract_file("vendor.img", map_partition("vendor"));" >> $scpt
+    echo "set_progress(1.000000);" >> $scpt
+    echo -e "\e[1;33mZipping........\e[0m"
+    for dir in tmp; do
+    ( cd "$dir" && zip -r ../"$gsi"_"$codename".zip . )
+    done
+    rm -r tmp
+    echo -e "\e[1;32mDone Flash "$gsi"_"$codename".zip\e[0m"
+    echo -e "\e[1;32mIn Custom Recovery\e[0m"
+    sleep 5
+    exit 1
+
+}
+check
+comp
+if ( mod==0)
+then
+    echo ""
+    echo -e "\e[1;31mCopy system.img to\e[0m"
+    echo -e "\e[1;31m "$pwd"\e[0m"
+    echo -e "\e[1;31mThen run this script again\e[0m"
+    sleep 2
+    exit 1
 else 
-echo
-echo -e "\e[1;31mCopy system.img to\e[0m"
-echo -e "\e[1;31m "$pwd"\e[0m"
-echo -e "\e[1;31mThen run this script again\e[0m"
+    if (mod!=2)
+    then mksys
+    else mkvendor
+    fi
 fi
 
 #####
