dab2c52f3a3984bf0862716180102810fc58e317
Bùi Đức Hiền <54226198+buihien224@users.noreply.github.com>
2022-04-05 11:46:24 +0700

    Update cus_add.sh

--- cus_add.sh
+++ cus_add.sh
@@ -19,20 +19,28 @@ haa=$(find $1)
 		done
 		
 }
-
+if [[ ! -f addon.img ]] ; then 
+	echo "Download addon.img"
+	wget https://github.com/buihien224/host/raw/main/addon.img
+fi
 
 linkcute=$(ls *.zip)
 if [[ -f $linkcute ]]; then
+	echo "Phat hien $linkcute"
 	mkdir working custom
 	dd if=/dev/zero bs=1M count=256 >> addon.img
 	sudo mount addon.img working
+	echo "Giai nen $linkcute"
 	cp $linkcute custom ; cd custom
 	jar xf $linkcute ; rm $linkcute
 	cd ..
+	echo "Copy file vao addon.img"
 	sudo cp -rf custom/* working
+	echo "Set quyen va SEcontest"
 	permis working
 	sudo umount working
 	sudo rm -rf working custom
 	resize2fs -f -M addon.img 
+	echo "Xong, hay copy file addon.img vao module/update"
 fi
 
