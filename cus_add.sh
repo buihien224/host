3edc2fdf9133954975cb1d529d862d494981179e
Bùi Đức Hiền <54226198+buihien224@users.noreply.github.com>
2022-04-05 11:53:14 +0700

    Update cus_add.sh

--- cus_add.sh
+++ cus_add.sh
@@ -27,20 +27,20 @@ fi
 linkcute=$(ls *.zip)
 if [[ -f $linkcute ]]; then
 	echo "Phat hien $linkcute"
-	sudo mkdir working custom
-	sudo dd if=/dev/zero bs=1M count=256 >> addon.img
+	mkdir working custom
+	dd if=/dev/zero bs=1M count=256 >> addon.img
 	sudo mount addon.img working
 	echo "Giai nen $linkcute"
 	cp $linkcute custom ; cd custom
-	sudo jar xf $linkcute ; rm $linkcute
+	jar xf $linkcute ; rm $linkcute
 	cd ..
 	echo "Copy file vao addon.img"
 	sudo cp -rf custom/* working
 	echo "Set quyen va SEcontest"
 	permis working
 	sudo umount working
-	sudo rm -rf working custom
-	sudo resize2fs -f -M addon.img 
+	rm -rf working custom
+	resize2fs -f -M addon.img 
 	echo "Xong, hay copy file addon.img vao module/update"
 fi
 
